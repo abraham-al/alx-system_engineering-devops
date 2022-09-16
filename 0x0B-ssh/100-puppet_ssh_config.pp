0c50ad23710aa30a659b3c689157209ee2f6eaa5
abraham-al <abraham.alemseged.f@gmail.com>
2022-09-16 22:51:45 +0300

    SSH

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Set up your client SSH configuration file so that you can connect to a server without typing a password using Puppet
+
+file_line { 'Identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+}
+
+file_line { 'No Passwdord needed':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+}
+
