898dbb4f768b4242a93dd52d78ff17c80da55b55
Abraham Alemseged <abraham.alemseged.f@gmail.com>
2022-11-16 09:55:30 +0300

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,5 +2,5 @@
 exec { 'file limit':
   onlyif   => 'test -e /etc/default/nginx',
   command  => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
-  provider => shell,
+  provider => shell
 }
